47ded37825ba9cea3fc41f9d0f298d4010e8af4f
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-10-24 23:19:19 +0800

    CHAPTER 4

---
+++ ch4/js/template/template.js
@@ -0,0 +1,16 @@
+export let URLGeoJson = "https://us-central1-gcpospasial.cloudfunctions.net/geozay";
+export let urlPostGCF = "https://us-central1-gcpospasial.cloudfunctions.net/gishttpzay";
+export let tableTag="tr";
+export let tableRowClass="content is-small";
+export let tableTemplate=`
+<td>#NAME#</td>
+<td >#KORDINAT#</td>
+<td>#TYPE#</td>
+`
+export const clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+X   : #X#<br>
+Y   : #Y#<br>
+HDMS : #HDMS#<br>
+`
